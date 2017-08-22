a4e2e92f7638dce1534ff0c797e400bbf4045f93
optikfluffel <optik@fluffel.io>
2017-08-22 22:43:17 +0200

    add ExVCR fixtures

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,13 @@
 use Mix.Config
 
 config :wunderground, api_key: System.get_env("WUNDERGROUND_API_KEY")
+
+if Mix.env == :test do
+  config :exvcr, [
+    filter_sensitive_data: [
+      [pattern: "(?<=http:\/\/api\\.wunderground\\.com\/api\/)(\\w+)(?=\/.+)", placeholder: "<<wunderground_api_key>>"]
+    ],
+    filter_url_params: true,
+    response_headers_blacklist: ["Set-Cookie"]
+  ]
+end
