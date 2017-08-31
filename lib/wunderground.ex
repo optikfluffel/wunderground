682413bdea9fcb1cf8db2e7f9faceafa7323f03d
optikfluffel <optik@fluffel.io>
2017-08-31 16:48:22 +0200

    tweak docs 📖

--- lib/wunderground.ex
+++ lib/wunderground.ex
@@ -13,28 +13,14 @@ defmodule Wunderground do
   @doc """
   Gets the current conditions for the given location.
 
-  ## Usage
+  ## Parameters
 
-      # For the US using {:us, state, city} or {:us_zip, zipcode}
+    - query: A `Wunderground.Query` that represents a location.
+
+  ## Example
+
       {:ok, conditions} = Wunderground.conditions({:us, "CA", "San_Francisco"})
-      {:ok, conditions} = Wunderground.conditions({:us_zip, 60290})
 
-      # International using {:international, country, city}
-      {:ok, conditions} = Wunderground.conditions({:international, "Australia", "Sydney"})
-
-      # Via coordinates using {:geo, lat, lng}
-      {:ok, conditions} = Wunderground.conditions({:geo, 37.8, -122.4})
-
-      # For an airport using {:airport, airport_code}
-      {:ok, conditions} = Wunderground.conditions({:airport, "KJFK"})
-
-      # For a specific personal weather station using {:pws, pws_id}
-      {:ok, conditions} = Wunderground.conditions({:pws, "KCASANFR70"})
-
-      # For the GeoIP location of the running machine using {:auto_ip}
-      # or any IP address using {:auto_ip, ip_adress}
-      {:ok, conditions} = Wunderground.conditions({:auto_ip})
-      {:ok, conditions} = Wunderground.conditions({:auto_ip, {185, 1, 74, 1}})
   """
   @spec conditions(Query.t) :: {:ok, Conditions.t} | {:error, API.error}
   defdelegate conditions(query), to: Conditions, as: :get
@@ -42,28 +28,14 @@ defmodule Wunderground do
   @doc """
   Gets the forecast for the given location.
 
-  ## Usage
+  ## Parameters
 
-      # For the US using {:us, state, city} or {:us_zip, zipcode}
-      {:ok, forecast} = Wunderground.forecast({:us, "CA", "San_Francisco"})
-      {:ok, forecast} = Wunderground.forecast({:us_zip, 60290})
+    - query: A `Wunderground.Query` that represents a location.
 
-      # International using {:international, country, city}
+  ## Example
+
       {:ok, forecast} = Wunderground.forecast({:international, "Australia", "Sydney"})
 
-      # Via coordinates using {:geo, lat, lng}
-      {:ok, forecast} = Wunderground.forecast({:geo, 37.8, -122.4})
-
-      # For an airport using {:airport, airport_code}
-      {:ok, forecast} = Wunderground.forecast({:airport, "KJFK"})
-
-      # For a specific personal weather station using {:pws, pws_id}
-      {:ok, forecast} = Wunderground.forecast({:pws, "KCASANFR70"})
-
-      # For the GeoIP location of the running machine using {:auto_ip}
-      # or any IP address using {:auto_ip, ip_adress}
-      {:ok, forecast} = Wunderground.forecast({:auto_ip})
-      {:ok, forecast} = Wunderground.forecast({:auto_ip, {185, 1, 74, 1}})
   """
   @spec forecast(Query.t) :: {:ok, Forecast.t} | {:error, API.error}
   defdelegate forecast(query), to: Forecast, as: :get
@@ -71,28 +43,14 @@ defmodule Wunderground do
   @doc """
   Gets the astronomy for the given location.
 
-  ## Usage
+  ## Parameters
 
-      # For the US using {:us, state, city} or {:us_zip, zipcode}
-      {:ok, astronomy} = Wunderground.astronomy({:us, "CA", "San_Francisco"})
-      {:ok, astronomy} = Wunderground.astronomy({:us_zip, 60290})
+    - query: A `Wunderground.Query` that represents a location.
 
-      # International using {:international, country, city}
-      {:ok, astronomy} = Wunderground.astronomy({:international, "Australia", "Sydney"})
+  ## Example
 
-      # Via coordinates using {:geo, lat, lng}
       {:ok, astronomy} = Wunderground.astronomy({:geo, 37.8, -122.4})
 
-      # For an airport using {:airport, airport_code}
-      {:ok, astronomy} = Wunderground.astronomy({:airport, "KJFK"})
-
-      # For a specific personal weather station using {:pws, pws_id}
-      {:ok, astronomy} = Wunderground.astronomy({:pws, "KCASANFR70"})
-
-      # For the GeoIP location of the running machine using {:auto_ip}
-      # or any IP address using {:auto_ip, ip_adress}
-      {:ok, astronomy} = Wunderground.astronomy({:auto_ip})
-      {:ok, astronomy} = Wunderground.astronomy({:auto_ip, {185, 1, 74, 1}})
   """
   @spec astronomy(Query.t) :: {:ok, Astronomy.t} | {:error, API.error}
   defdelegate astronomy(query), to: Astronomy, as: :get
@@ -100,28 +58,15 @@ defmodule Wunderground do
   @doc """
   Gets the almanac for the given location.
 
-  ## Usage
+  ## Parameters
 
-      # For the US using {:us, state, city} or {:us_zip, zipcode}
-      {:ok, almanac} = Wunderground.almanac({:us, "CA", "San_Francisco"})
-      {:ok, almanac} = Wunderground.almanac({:us_zip, 60290})
+    - query: A `Wunderground.Query` that represents a location.
 
-      # International using {:international, country, city}
-      {:ok, almanac} = Wunderground.almanac({:international, "Australia", "Sydney"})
-
-      # Via coordinates using {:geo, lat, lng}
-      {:ok, almanac} = Wunderground.almanac({:geo, 37.8, -122.4})
+  ## Example
 
       # For an airport using {:airport, airport_code}
       {:ok, almanac} = Wunderground.almanac({:airport, "KJFK"})
 
-      # For a specific personal weather station using {:pws, pws_id}
-      {:ok, almanac} = Wunderground.almanac({:pws, "KCASANFR70"})
-
-      # For the GeoIP location of the running machine using {:auto_ip}
-      # or any IP address using {:auto_ip, ip_adress}
-      {:ok, almanac} = Wunderground.almanac({:auto_ip})
-      {:ok, almanac} = Wunderground.almanac({:auto_ip, {185, 1, 74, 1}})
   """
   @spec almanac(Query.t) :: {:ok, Almanac.t} | {:error, API.error}
   defdelegate almanac(query), to: Almanac, as: :get
